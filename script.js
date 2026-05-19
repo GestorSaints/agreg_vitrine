d2f77b1e5dd4a46f9e225533f1b4ccabc0170fe7
GestorSaints <saints.gestor@gmail.com>
2026-05-19 16:17:49 -0300

    Novas mudanças

--- script.js
+++ script.js
@@ -1,126 +1,30 @@
 
-let currentSlide = 0;
+setInterval(changeSlide, 4000);
+document.addEventListener("DOMContentLoaded", () => {
+  const slides = document.querySelectorAll(".slide");
+  let currentSlide = 0;
 
-const slidesContainer =
-document.getElementById('slides');
+  function changeSlide() {
+    slides[currentSlide].classList.remove("active");
 
-const productsGrid =
-document.getElementById('products-grid');
+    currentSlide = (currentSlide + 1) % slides.length;
 
-let totalSlides = 0;
+    slides[currentSlide].classList.add("active");
+  }
 
-/* CARREGAR PRODUTOS */
-
-fetch('produtos.json')
-
-.then(resposta => resposta.json())
-
-.then(produtos => {
-
-  /* GERAR SLIDES */
-
-  produtos.forEach(produto => {
-
-    slidesContainer.innerHTML += `
-
-      <div class="slide">
-
-        <img src="${produto.imagem}">
-
-        <div class="slide-overlay">
-
-          <div class="slide-content">
-
-            <h2>
-              ${produto.nome}
-            </h2>
-
-            <p>
-              Coleção AGREG Premium
-            </p>
-
-          </div>
-
-        </div>
-
-      </div>
-
-    `;
-
-  });
-
-  /* CONTAR SLIDES */
-
-  totalSlides =
-  document.querySelectorAll('.slide').length;
-
-  /* GERAR PRODUTOS */
-
-  produtos.forEach(produto => {
-
-    productsGrid.innerHTML += `
-
-      <div class="product-card">
-
-        <img src="${produto.imagem}">
-
-        <div class="product-info">
-
-          <h3>
-            ${produto.nome}
-          </h3>
-
-          <p>
-            ${produto.preco.toLocaleString('pt-BR', {
-              style:'currency',
-              currency:'BRL'
-            })}
-          </p>
-
-          <button>
-            Ver Produto
-          </button>
-
-        </div>
-
-      </div>
-
-    `;
-
-  });
-
-  /* AUTO SLIDE */
-
-  setInterval(nextSlide, 5000);
-
+  setInterval(changeSlide, 4000);
 });
 
-/* FUNÇÕES DO CAROUSEL */
 
-function updateSlide(){
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
 
-  slidesContainer.style.transform =
-  `translateX(-${currentSlide * 100}%)`;
+function changeSlide() {
+  slides[currentSlide].classList.remove("active");
+
+  currentSlide = (currentSlide + 1) % slides.length;
+
+  slides[currentSlide].classList.add("active");
 }
 
-function nextSlide(){
-
-  currentSlide++;
-
-  if(currentSlide >= totalSlides){
-    currentSlide = 0;
-  }
-
-  updateSlide();
-}
-
-function prevSlide(){
-
-  currentSlide--;
-
-  if(currentSlide < 0){
-    currentSlide = totalSlides - 1;
-  }
-
-  updateSlide();
-}
+// inicia o carrossel
